0f9fdea70b36274cf50894f7bca87a14b5e878d6
Yann Renaudin <renaudin.yann@gmail.com>
2018-10-11 05:58:57 -0400

    feat: style toolbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -7,18 +7,22 @@ const StyledH2 = styled.h2`
   margin: 12px 0px;
 `;
 
+const StyledToolbar = styled(Toolbar)`
+  background-color: var(--mdc-theme-accent1);
+`;
+
 class TopBar extends Component {
   render() {
     return (
-      <Toolbar>
+      <StyledToolbar>
         <ToolbarRow>
           <ToolbarTitle>
-            <Typography use="headline5" theme="secondary">
+            <Typography use="headline5" theme="primary">
               <StyledH2> Morphism Playground</StyledH2>
             </Typography>
           </ToolbarTitle>
         </ToolbarRow>
-      </Toolbar>
+      </StyledToolbar>
     );
   }
 }
