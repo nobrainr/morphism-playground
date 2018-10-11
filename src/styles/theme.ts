f566aae45ccc0f3deabcd48f60fcd2ff5b8a86c6
Yann Renaudin <renaudin.yann@gmail.com>
2018-10-11 05:57:40 -0400

    feat: update theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,10 @@
+// https://colorhunt.co/palette/361
 export const theme = {
-  primary: '#303030',
-  secondary: '#f8cf47',
+  primary: '#f9ed69',
+  secondary: '#f08a5d',
+  accent1: '#303030',
+  error: '#b83b5e',
+  bg: '#303030',
   onPrimary: '#ffffff'
 };
 
