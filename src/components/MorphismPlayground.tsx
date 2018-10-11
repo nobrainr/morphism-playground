f407dab19ba61ce9bac4e77f02c267b8b15a37fb
Yann Renaudin <renaudin.yann@gmail.com>
2018-10-11 05:58:31 -0400

    feat: Add AlertIcon when error occurs

--- src/components/MorphismPlayground.tsx
+++ src/components/MorphismPlayground.tsx
@@ -5,12 +5,13 @@ import { Schema } from './Schema';
 import { Target } from './Target';
 import { SourceSchemaProvider, SourceSchemaContext } from './SourceSchemaProvider';
 import { Typography } from '@rmwc/typography';
+import { CheckIcon, AlertCircleOutlineIcon } from 'mdi-react';
 
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
-  border-top: 5px solid var(--mdc-theme-secondary);
+  border-top: 5px solid var(--mdc-theme-primary);
 `;
 
 const PlaygroundsContainer = styled.div`
@@ -19,9 +20,13 @@ const PlaygroundsContainer = styled.div`
 `;
 const MorphingStatus = styled(Typography).attrs({
   use: 'caption',
-  theme: 'secondary-bg on-secondary'
+  theme: 'primary-bg on-secondary'
 })`
   padding: 4px;
+`;
+
+const StyledAlertIcon = styled(AlertCircleOutlineIcon)`
+  margin-right: 12px;
 `;
 
 const OutputContainer = styled.div`
@@ -57,10 +62,14 @@ const PlaygroundHeader = styled.header`
   border-bottom: 1px solid rgba(255, 255, 255, 0.05);
 `;
 
-const StyledCaption = styled(Typography).attrs({
+const StyledCaption = styled(Typography).attrs<any>({
   use: 'caption',
-  theme: 'secondary'
-})``;
+  theme: props => (props.error ? 'error' : 'primary')
+})`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 interface IDivider {
   direction: 'horizontal' | 'vertical';
@@ -68,7 +77,7 @@ interface IDivider {
 const Divider = styled<IDivider, any>('div')`
   width: ${props => (props.direction === 'horizontal' ? '' : '20px')};
   height: ${props => (props.direction === 'horizontal' ? '20px' : '')};
-  background-color: ${props => (props.direction === 'horizontal' ? 'var(--mdc-theme-primary)' : 'var(--mdc-theme-primary)')};
+  background-color: ${props => (props.direction === 'horizontal' ? 'var(--mdc-theme-accent1)' : 'var(--mdc-theme-accent1)')};
   border-left: 1px solid rgba(48, 48, 48, 0.1);
   border-right: 1px solid rgba(48, 48, 48, 0.4);
   display: flex;
@@ -90,7 +99,15 @@ export class MorphismViz extends Component {
                       <Typography use="headline6" theme="onPrimary">
                         Source (JSON)
                       </Typography>
-                      <StyledCaption>{errors.source}</StyledCaption>
+                      <StyledCaption error={errors.source ? 1 : 0}>
+                        {errors.source ? (
+                          <>
+                            <StyledAlertIcon /> {errors.source}
+                          </>
+                        ) : (
+                          <CheckIcon />
+                        )}
+                      </StyledCaption>
                     </PlaygroundHeader>
                     <InputSource />
                   </PlaygroundContainer>
@@ -100,7 +117,16 @@ export class MorphismViz extends Component {
                       <Typography use="headline6" theme="onPrimary">
                         Schema (JS)
                       </Typography>
-                      <StyledCaption>{errors.schema}</StyledCaption>
+                      <StyledCaption error={errors.schema ? 1 : 0}>
+                        {errors.schema ? (
+                          <>
+                            {' '}
+                            <StyledAlertIcon /> {errors.schema}
+                          </>
+                        ) : (
+                          <CheckIcon />
+                        )}
+                      </StyledCaption>
                     </PlaygroundHeader>
                     <Schema />
                   </PlaygroundContainer>
@@ -112,7 +138,7 @@ export class MorphismViz extends Component {
                       Target
                     </Typography>
                     <div>
-                      <Typography use="caption" theme="secondary">
+                      <Typography use="caption" theme="primary">
                         Morphed {stats.numberOfItems} {stats.numberOfItems > 1 ? 'items' : 'item'} successfully in
                         <MorphingStatus>
                           {stats.lastRunElapsedTime}
